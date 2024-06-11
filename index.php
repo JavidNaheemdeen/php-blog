1d301579a5a390ab318831deaa4a238f213db85d
javid <javidnaheem2001@gmail.com>
2024-06-11 16:47:39 +0530

    login created

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include("includes/header.php") ?>
+<?php include("includes/navbar.php") ?>
+
+<div class="py-5">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h1 class="display-4">home</h1>
+            <button class="btn btn-primary">Login</button>
+         </div>
+      </div>
+   </div>
+</div>
+
+<?php include("includes/footer.php") ?>
---
+++ register.php
@@ -0,0 +1,39 @@
+<?php include("includes/header.php") ?>
+<?php include("includes/navbar.php") ?>
+
+<div class="py-5">
+   <div class="container">
+      <div class="row justify-content-center">
+         <div class="col-md-5">
+            <div class="card">
+               <div class="card-header">
+                  <h3 class="text-center">Register</h3>
+               </div>
+               <div class="card-body">
+                  <div class="form-group mb-3">
+                     <label>First Name</label>
+                     <input type="email" placeholder="first name" class="form-control">
+                  </div>
+                  <div class="form-group mb-3">
+                     <label>Last Name</label>
+                     <input type="email" placeholder="last name" class="form-control">
+                  </div>
+                  <div class="form-group mb-3">
+                     <label>Email ID</label>
+                     <input type="email" placeholder="email address" class="form-control">
+                  </div>
+                  <div class="form-group mb-3">
+                     <label>Password</label>
+                     <input type="password" placeholder="password" class="form-control">
+                  </div>
+                  <div class="form-group mb-3">
+                     <button type="submit" class="btn btn-primary">Register Now</button>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+<?php include("includes/footer.php") ?>
